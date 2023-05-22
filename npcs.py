99efbc5a6a1b3a7abb8ecb3803c5d90c101683b3
Yevhenii Kozii <varmid@gmail.com>
2023-05-23 02:53:53 +0300

    parse npcs from wowhead htmls

--- npcs.py
+++ npcs.py
@@ -1,60 +1,86 @@
 import requests
 import multiprocessing
 from lxml import html
+from slpp import slpp as lua
 
 WOWHEAD_URL = 'https://wowhead.com'
 
 class NPC:
-    def __init__(self, id, name, locations = []):
+    def __init__(self, id, name, desc):
         self.id = id
         self.name = name
-        self.locations = locations
+        self.desc = desc
 
     def __str__(self):
         name_txt = str(self.name).replace('"', '""')
-        return f'{self.id}\t"{name_txt}"\t"{self.locations}"'
+        desc_txt = str(self.desc).replace('"', '""')
+        return f'{self.id}\t"{name_txt}"\t"{desc_txt}"'
+    
+def get_all_wowhead_npc_ids() -> list:
+    with open(f'wowhead_npc_ids.csv', 'r') as input_file:
+        all_ids_str = input_file.readline()
+    return list(map(lambda x: int(x), all_ids_str.split(', ')))
 
+def save_npc_page(id) -> str:
+    url = WOWHEAD_URL + f'/classic/npc={id}'
+    r = requests.get(url)
+    with open(f'npc_htmls/{id}.html', 'w') as output_file:
+        output_file.write(r.text)
+
+def save_npc_htmls_from_wowhead():
+    all_ids = get_all_wowhead_npc_ids()
+
+    with multiprocessing.Pool(64) as p:
+        p.map(save_npc_page, all_ids)
 
 def parse_npc_page(id, page) -> NPC:
     tree = html.fromstring(page)
     npc_name = tree.xpath('//h1')[0].text
-    if (npc_name == "Classic NPCs"):
-        return NPC(id, "null", 'null')
-    npc_locations_element = tree.xpath("/html[@class='js-focus-visible pointer-device']/body[@class='standard-layout locale-0 has-ads webp']/div[@class='layout-wrapper']/div[@class='layout']/div[@id='page-content']/div[@id='main']/div[@id='main-contents']/div[@class='text']/div[3]")
-    if (not npc_locations_element):
-        return NPC(id, npc_name, 'null')
+    if (npc_name == "Classic NPCs" or npc_name is None): # 15384, 15672, 17689, 17690, 17696, 17698 are corrupt and not used
+        print(f'{id} is null')
+        return NPC(id, '', '')
+    splitted_name = npc_name.split(' <')
+    if (len(splitted_name) == 1):
+        return NPC(id, npc_name, '')
+    elif (len(splitted_name) == 2):
+        return NPC(id, splitted_name[0], '<'+splitted_name[1])
+    else:
+        print(f'error for {id}')
+        return NPC(id, 'error', 'error')
 
-    npc_locations = npc_locations_element[0].xpath('.//a')
-    npc_locations_str = ",".join(list(map(lambda x: x.text, npc_locations)))
-    return NPC(id, npc_name, npc_locations_str)
-
-def parse_npc(id) -> str:
-    url = WOWHEAD_URL + f'/classic/npc={id}'
-    r = requests.get(url)
-    return str(parse_npc_page(id, r.content)) + "\n"
+def parse_npc(id) -> NPC:
+    with open(f'npc_htmls/{id}.html', 'r') as input_file:
+        page_content = input_file.read()
+    return parse_npc_page(id, page_content)
 
 
-def parse_all_items_from_wowhead():
-    with open(f'wowhead_npc_ids.csv', 'r') as input_file:
-        all_ids_str = input_file.readline()
-    all_ids = list(map(lambda x: int(x), all_ids_str.split(', ')))
+def parse_all_npcs_from_wowhead_htmls():
+    all_ids = get_all_wowhead_npc_ids()
 
-    with multiprocessing.Pool(128) as p:
-        with open(f'npcs_res.tsv', 'w') as output_file:
-            output_file.writelines(p.map(parse_npc, all_ids))
+    with open(f'npcs_res.tsv', 'w') as output_file:
+        output_file.write('ID\tEN Name\tEN Desc\n')
+        for id in all_ids:
+            # print(id)
+            npc = parse_npc(id)
+            if (npc.name):
+                output_file.write(str(npc) + '\n')
 
-# print(parse_npc(3690))
+def parse_npcs_lua():
+    with open('entries/npc.lua', 'r') as input_file:
+        lua_file = input_file.read()
+        decoded_npcs_lua = lua.decode(lua_file)
+        # item_117 = decoded_npcs_lua[117]
+        # print(item_117)
+        reencoded_npcs_lua = lua.encode(decoded_npcs_lua)
+        # print(reencoded_items_lua)
+        with open(f'npcs_temp.lua', 'w') as output_file:
+            output_file.writelines(reencoded_npcs_lua)
 
-# with open('npcs.csv', 'w') as output_file:
-#     for i in  range(6946, 50000):
-#         npc = str(parse_npc(i))
-#         print(npc)
-#         output_file.write(npc+'\n')
 
 if __name__ == '__main__':
-    # parse_all_items_from_wowhead()
-    with open(f'npcs_res1.tsv', 'w') as output_file:
-        output_file.writelines(parse_npc(18199))
+    # save_npc_htmls_from_wowhead()
+    parse_all_npcs_from_wowhead_htmls()
+    # parse_npcs_lua()
 
 
 
