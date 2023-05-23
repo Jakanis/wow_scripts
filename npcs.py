280bcd0db115071a44205f32c6ca890dff459798
Yevhenii Kozii <varmid@gmail.com>
2023-05-24 02:12:56 +0300

    parse npc.lua

--- npcs.py
+++ npcs.py
@@ -16,6 +16,21 @@ class NPC:
         desc_txt = str(self.desc).replace('"', '""')
         return f'{self.id}\t"{name_txt}"\t"{desc_txt}"'
     
+class NPC_TR:
+    def __init__(self, id, name_en, desc_en, name_ua, desc_ua):
+        self.id = id
+        self.name_en = name_en
+        self.desc_en = desc_en
+        self.name_ua = name_ua
+        self.desc_ua = desc_ua
+
+    def __str__(self):
+        name_en_txt = str(self.name_en).replace('"', '""')
+        desc_en_txt = str(self.desc_en).replace('"', '""')
+        name_ua_txt = str(self.name_ua).replace('"', '""')
+        desc_ua_txt = str(self.desc_ua).replace('"', '""')
+        return f'{self.id}\t"{name_en_txt}"\t"{desc_en_txt}"\t"{name_ua_txt}"\t"{desc_ua_txt}"'
+    
 def get_all_wowhead_npc_ids() -> list:
     with open(f'wowhead_npc_ids.csv', 'r') as input_file:
         all_ids_str = input_file.readline()
@@ -56,31 +71,42 @@ def parse_npc(id) -> NPC:
 
 def parse_all_npcs_from_wowhead_htmls():
     all_ids = get_all_wowhead_npc_ids()
-
+    all_npcs = dict()
     with open(f'npcs_res.tsv', 'w') as output_file:
         output_file.write('ID\tEN Name\tEN Desc\n')
         for id in all_ids:
-            # print(id)
             npc = parse_npc(id)
             if (npc.name):
-                output_file.write(str(npc) + '\n')
+                all_npcs[id]=parse_npc(id)
+            #     output_file.write(str(npc) + '\n')
+    return all_npcs
 
 def parse_npcs_lua():
+    import re
+    all_npcs = dict()
     with open('entries/npc.lua', 'r') as input_file:
         lua_file = input_file.read()
-        decoded_npcs_lua = lua.decode(lua_file)
-        # item_117 = decoded_npcs_lua[117]
-        # print(item_117)
-        reencoded_npcs_lua = lua.encode(decoded_npcs_lua)
-        # print(reencoded_items_lua)
-        with open(f'npcs_temp.lua', 'w') as output_file:
-            output_file.writelines(reencoded_npcs_lua)
-
+    for line in lua_file.split('\n'):
+        res = re.findall('\[(\d+)\] = { \"(.+)\".+-- (.+)', line)[0]
+        if (len(res) != 3):
+            print(f'check {line}')
+        id = int(res[0])
+        splitted_name_ua = res[1].split('", "')
+        name_ua = splitted_name_ua[0]
+        desc_ua = f'<{splitted_name_ua[1]}>' if len(splitted_name_ua) == 2 else ''
+        splitted_name_en = res[2].split(' <')
+        name_en = splitted_name_en[0]
+        desc_en = f'<{splitted_name_en[1]}' if len(splitted_name_en) == 2 else ''
+        npc = NPC_TR(id, name_en, desc_en, name_ua, desc_ua)
+        all_npcs[id] = npc
+    return all_npcs
 
 if __name__ == '__main__':
     # save_npc_htmls_from_wowhead()
-    parse_all_npcs_from_wowhead_htmls()
-    # parse_npcs_lua()
+    # npcs_from_wowhead = parse_all_npcs_from_wowhead_htmls()
+    
+    npcs_from_lua = parse_npcs_lua()
+
 
 
 
