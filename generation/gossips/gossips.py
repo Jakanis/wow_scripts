5a0d01acf25d72738411730ae567300a5f112991
Yevhenii Kozii <varmid@gmail.com>
2025-04-23 00:16:15 +0300

    Big gossips update
    Imported all gossips from current feedbacks
    Added support for several .pkl files (because I've lost some feedbacks and that's all I have left)
    Copy-pasted checks from chats
    Store raw gossips to figure out (later) where we go common gossips from

--- generation/gossips/gossips.py
+++ generation/gossips/gossips.py
@@ -1,3 +1,5 @@
+import os
+
 from generation.npc.npc import load_npcs_from_db, NPC_MD
 from generation.utils.utils import compare_directories
 
@@ -24,20 +26,26 @@ def __clean_newlines(value: str) -> str:
 def load_missing_gossips() -> list[Gossip]:
     import pickle
     gossips = list()
-    with open('input/missing_gossips.pkl', 'rb') as f:
-        missing_gossips = pickle.load(f)
-        for npc_id, npc_gossips in missing_gossips.items():
-            for gossip_key, gossip_object in npc_gossips.items():
-                gossip_text = None
-                if type(gossip_object) == str:
-                    gossip_text = gossip_object
-                elif type(gossip_object) == dict:
-                    gossip_text = gossip_object[0]
-                else:
-                    print(f"Warning! Unknown gossip_object type: {type(gossip_object)}")
-                    continue
-                gossip = Gossip(npc_id, __clean_newlines(gossip_text), gossip_key=gossip_key)
-                gossips.append(gossip)
+
+    feedbacks_folder = 'input/feedbacks'
+    for feedback_file in os.listdir(feedbacks_folder):
+        feedback_file_path = os.path.join(feedbacks_folder, feedback_file)
+        if os.path.isfile(feedback_file_path) and feedback_file.endswith('.pkl'):
+            with open(feedback_file_path, 'rb') as f:
+                missing_gossips = pickle.load(f)
+                for npc_id, npc_gossips in missing_gossips.items():
+                    for gossip_key, gossip_object in npc_gossips.items():
+                        gossip_text = None
+                        if type(gossip_object) == str:
+                            gossip_text = gossip_object
+                        elif type(gossip_object) == dict:
+                            gossip_text = gossip_object[0]
+                        else:
+                            print(f"Warning! Unknown gossip_object type: {type(gossip_object)}")
+                            continue
+                        gossip = Gossip(npc_id, __clean_newlines(gossip_text).strip(), gossip_key=gossip_key)
+                        if not gossip in gossips:
+                            gossips.append(gossip)
     return gossips
 
 
@@ -56,10 +64,35 @@ def populate_npcs(gossips: list[Gossip], npcs: dict[int, dict[str, NPC_MD]]):
     return gossips
 
 
+def is_value_regex_in_set(gossip: Gossip, collection: set[str], result: list[Gossip] = None):
+    import re
+    if collection is None:
+        return False
+    if gossip.text in collection:
+        return True
+    for key in collection:
+        re_template = r'.+?'
+        if "<" in key:
+            key_pattern = '^' + re.sub('<.+?>', re_template, re.escape(key)) + '$'
+            if key_pattern != r'^.+?$' and re.match(key_pattern, gossip.text):
+                return True
+        if "<" in gossip.text:
+            chat_pattern = '^' + re.sub('<.+?>', re_template, re.escape(gossip.text)) + '$'
+            if re.match(chat_pattern, key):
+                collection.remove(key)
+                if result and gossip in result:
+                    result.remove(gossip)
+                collection.add(key)
+                return False
+
+    return False
+
+
 def cleanup_gossips(gossips: list[Gossip]):
     existing_gossips = set()
     existing_common_texts = set()
     existing_texts: dict[str, int] = dict()
+    existing_texts_by_npc: dict[str, set[str]] = dict()
 
     filtered_gossips = list()
     for gossip in gossips:
@@ -69,18 +102,19 @@ def cleanup_gossips(gossips: list[Gossip]):
             continue
         if gossip in existing_gossips:
             continue
-        if gossip.text in existing_texts:
-            print(f'Gossip text "{gossip.text}" already exist.')
+        if is_value_regex_in_set(gossip, existing_common_texts):
+            continue
+        if gossip.npc_id in existing_texts_by_npc and is_value_regex_in_set(gossip, existing_texts_by_npc[gossip.npc_id], filtered_gossips):
+            continue
         if gossip.npc_name == 'common':
             existing_common_texts.add(gossip.text)
+        existing_texts[gossip.text] = existing_texts.get(gossip.text, 0) + 1
         existing_gossips.add(gossip)
-        if gossip.text in existing_texts:
-            existing_texts[gossip.text] += 1
-        else:
-            existing_texts[gossip.text] = 0
+        existing_texts_by_npc[gossip.npc_id] = existing_texts_by_npc.get(gossip.npc_id, set())
+        existing_texts_by_npc[gossip.npc_id].add(gossip.text)
         filtered_gossips.append(gossip)
 
-    text_repeatance = list(sorted(existing_texts.items(), key=lambda item: item[1], reverse=True))
+    text_repeatance = list(sorted(filter(lambda x: x[1] > 1, existing_texts.items()), key=lambda item: item[1], reverse=True))
 
     return filtered_gossips
 # filtered_gossips[67]
@@ -243,7 +277,7 @@ def generate_sources(gossips: list[Gossip]):
         if gossip.npc_id == 0:
             path = f'output/source_for_crowdin/gossip{suffix}/{gossip.npc_name}.xml'
         else:
-            path = f'output/source_for_crowdin/gossip{suffix}/{gossip.npc_name.capitalize()[:1]}/{__gossip_filename(gossip)}.xml'
+            path = f'output/source_for_crowdin/gossip{suffix}/{__gossip_filename(gossip).capitalize()[:1]}/{__gossip_filename(gossip)}.xml'
 
         gossips_by_path[path] = gossips_by_path.get(path, list())
         gossips_by_path[path].append(gossip)
@@ -256,6 +290,28 @@ def generate_sources(gossips: list[Gossip]):
     print(f'Generated {count} gossips.')
 
 
+def group_gossips_by_npcs(gossips: list[Gossip]) -> dict[(str, int), list[Gossip]]:
+    npcs_to_gossips: dict[(str, int), list[Gossip]] = dict()
+    for gossip in gossips:
+        npc_key = (gossip.npc_name, gossip.npc_id)
+        npcs_to_gossips[npc_key] = npcs_to_gossips.get(npc_key, list())
+        npcs_to_gossips[npc_key].append(gossip)
+    return npcs_to_gossips
+
+def verify_duplicates(gossips: list[Gossip]):
+    gossips_by_npcs = group_gossips_by_npcs(gossips)
+    common_chat_texts = map(lambda x: x.text, gossips_by_npcs.get(('common', 0)))
+    for npc_key, gossips in gossips_by_npcs.items():
+        npc_texts = set()
+        for gossip in gossips:
+            if gossip.text in npc_texts:
+                print(f'Warning! Gossip duplicated for NPC "{npc_key}": {gossip.text}')
+            npc_texts.add(gossip.text)
+
+            if npc_key != ('common', 0) and gossip.text in common_chat_texts:
+                print(f'Warning! Gossip text already exist in common for NPC "{npc_key}": {gossip.text}')
+
+
 if __name__ == '__main__':
     crowdin_gossips = load_from_db('crowdin_gossips.db')
     missing_gossips = load_missing_gossips()
@@ -264,11 +320,13 @@ if __name__ == '__main__':
 
     combined_gossips = cleanup_gossips([*crowdin_gossips, *missing_gossips])
 
+    save_to_db([*crowdin_gossips, *missing_gossips], 'raw_gossips.db')
     # save_to_db(missing_gossips, 'gossips.db')
     save_to_db(combined_gossips, 'gossips.db')
 
-    gossips = load_from_db('gossips.db')
+    # gossips = load_from_db('gossips.db')
 
+    verify_duplicates(crowdin_gossips)
     generate_sources(crowdin_gossips)
 
     diffs = compare_directories('input/source_from_crowdin', 'output/source_for_crowdin')
